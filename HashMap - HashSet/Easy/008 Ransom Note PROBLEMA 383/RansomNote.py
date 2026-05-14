f2881419d9409b99d20950988308bba293a18615
Dainichi1 <torquati79@yahoo.it>
2026-05-14 19:35:26 +0200

    problema 383 Ransom note Python

---
+++ HashMap - HashSet/Easy/008 Ransom Note PROBLEMA 383/RansomNote.py
@@ -0,0 +1,24 @@
+from operator import index
+from traceback import format_tb
+
+
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        if len(ransomNote) > len(magazine):
+            return False
+
+        counts = [0] *26
+
+        for c in magazine:
+            counts[ord(c) - 97] += 1
+
+        for c in ransomNote:
+            index = ord(c) - 97
+            if counts[index] == 0:
+                return False
+            counts[index] -= 1
+
+        return True
+
+# ransomNote = "aa"
+# magazine = "aab
